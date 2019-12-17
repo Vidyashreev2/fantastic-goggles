9130e6a1479da37eef6471ff8ddf6c252192aca0
Vidyashree Bhat <vidyashre.bhat@v2solutions.com>
2019-12-17 15:57:42 +0530

    Loop file editted

--- loop.sh
+++ loop.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 echo"THis is loop file"
-for i in 0,1,2,3,4
-echo $i
+for i in 1 2 3 4 
+do
+echo "Looping is $i"
+done
+input = "Hello"
+while [ "$input" != "bye" ]
+do 
+echo "While loop"
+done
